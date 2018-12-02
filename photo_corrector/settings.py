f905b17a56ac20808ab0ca12a8716c46efa6f3b2
Ubuntu <dimjava@sphereprojectin.adaqzc1fu33urh31dvuuujltaa.jx.internal.cloudapp.net>
2018-12-02 14:15:01 +0000

    Added blumix.me to allowed hosts

--- photo_corrector/settings.py
+++ photo_corrector/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '+f(rn8t1fikarx7t0-0g-fk+e!(1)m$0_2lkkrpn3kh$yv+4i^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["104.214.52.178", "127.0.0.1", "185.5.138.253", "localhost"]
+ALLOWED_HOSTS = ["104.214.52.178", "127.0.0.1", "185.5.138.253", "localhost", "blumix.me"]
 
 
 # Application definition
